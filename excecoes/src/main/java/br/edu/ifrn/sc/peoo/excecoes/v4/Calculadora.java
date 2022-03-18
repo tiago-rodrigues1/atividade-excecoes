31525dc7c1dc9c4765fe6bddb74c2d6ece4e03e5
Tiago Rodrigues <70401246+tiago-rodrigues1@users.noreply.github.com>
2022-03-18 10:17:35 -0300

    Add files via upload

---
+++ excecoes/src/main/java/br/edu/ifrn/sc/peoo/excecoes/v4/Calculadora.java
@@ -0,0 +1,9 @@
+package br.edu.ifrn.sc.peoo.excecoes.v4;
+
+public class Calculadora {
+
+    public static int dividir(int dividendo, int divisor) {
+        int quosciente = dividendo / divisor;
+        return quosciente;
+    }
+}
